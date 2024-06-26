e70f2c7d4f994561dab1975a8ae4fc7866a8694d
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2024-06-25 18:10:56 -0700

    Removed unused component from imports

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { FooterOverlay, Newsletter, SubHeading } from '../../components';
+import { FooterOverlay, Newsletter } from '../../components';
 import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
 import './Footer.css';
 import { images } from '../../constants';
